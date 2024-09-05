f4921a6680d4fe409c98defc05f394c852b957ad
Auttapol <auttaplon.cha@mahidol.ac.th>
2024-09-05 12:06:25 +0700

    update template

--- kms/settings.py
+++ kms/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'bootstrap5',
     'permission_app', 
     'content_app', 
 ]
